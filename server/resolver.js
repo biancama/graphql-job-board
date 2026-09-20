6d436159adfe17bec32113205d10e4d71bc12722
Massimo Biancalani <massimo.biancalani@ing.com>
2026-09-20 16:24:42 +0200

    Returning a mock array of data

--- server/resolver.js
+++ server/resolver.js
@@ -1,5 +1,20 @@
 export const resolvers = {
     Query: {
         greeting: () => 'Hello world' ,
+        jobs: () => {
+            return [
+                {
+                id : 'test-id01',
+                title: 'The Title 01',
+                description: 'Thhe description',
+            },
+                            {
+                id : 'test-id02',
+                title: 'The Title 02',
+                description: 'Thhe description',
+            }
+
+        ]
+        }
     },
 }
